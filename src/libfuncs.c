e48fefe4ecec83f7d4e7b3baa2e79b4479be7f32
Parsa <Parsa@mail>
2024-12-05 17:40:20 +0330

    .

---
+++ src/libfuncs.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+float add(float x, float y) {
+    return x + y;
+}
+
+
+float sub(float x, float y) {
+    return x - y;
+}
+
+
+float mult(float x, float y) {
+    return x * y;
+}
+
+
+float divide(float x, float y) {
+    return x / y;
+}
